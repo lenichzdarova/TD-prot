a1b1a539211530acbd688b5d403edc637e37a403
lenichzdarova <lenichzdarova@gmail.com>
2023-01-13 23:47:43 +0300

    base implementation of upgades
    No agile. only attack stats decorators

---
+++ Assets/Scripts/TowerUpgrade.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class TowerUpgrades
+{
+    public readonly TowerType _towerType;
+    private List <IStatsProvider<AttackStats>> _statsProvider;
+
+    public TowerUpgrades (TowerType towerType)
+    {
+        _towerType = towerType;
+        _statsProvider = new List<IStatsProvider<AttackStats>>(5); //because of list ompimization LUL
+
+        switch (_towerType)
+        {
+            //create all upgrades decorators for each tower;
+        }
+    }
+
+    public IStatsProvider<AttackStats>[] GetUpgrades(int upgradesLevel)
+    {        
+        var upgrades = new IStatsProvider<AttackStats>[upgradesLevel];
+        _statsProvider.CopyTo(0, upgrades, 0, upgradesLevel);
+        return upgrades;
+    }
+}
---
+++ Assets/Scripts/TowerUpgradesHandler.cs
@@ -0,0 +1,28 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+
+public class TowerUpgradesHandler
+{
+    private static Dictionary<TowerType, int> _upgradesLevel;
+    private static List<TowerUpgrades> _upgrades;
+
+    public TowerUpgradesHandler()
+    {
+        //cycle towerTypes to inialize all upgrades;
+    }
+
+    public static IStatsProvider<AttackStats>[] GetUpgrades(TowerType towerType)
+    {     
+        foreach (var upgrade in _upgrades)
+        {
+            int upgradesLevel = _upgradesLevel[towerType];            
+            if (upgrade._towerType == towerType)
+            {
+                return upgrade.GetUpgrades(upgradesLevel);
+            }
+        }
+        throw new NotImplementedException($"Tower type {towerType} upgrades not implemented");
+    }
+}
