09a40f117ab2af72cd3a693fed90fe8659574f26
lenichzdarova <lenichzdarova@gmail.com>
2023-01-19 23:24:26 +0300

    GameSceneManager Impl/ gamelauncher scene flow impl

--- Assets/Scripts/GameLaunch/GameLauncher.cs
+++ Assets/Scripts/GameLaunch/GameLauncher.cs
@@ -5,12 +5,33 @@ public class GameLauncher : MonoBehaviour
 {
     [SerializeField]
     private MainMenu _mainMenu;
-    private PlayerPersistentData _persistentData;
+    private DataManager _dataManager;
+    private GameSceneManager _gameSceneManager;
 
     private void Awake()
     {
-        var dataManager = new DataManager();
-        _persistentData = dataManager.LoadData();
-        _mainMenu.Initialize(_persistentData);
-    }    
+        _dataManager = new DataManager();
+        var playerData = _dataManager.LoadData();        
+        _mainMenu.Initialize(playerData);
+        _gameSceneManager = new GameSceneManager();
+        _mainMenu.ContinueClick += OnCountinueClick;
+        _mainMenu.NewGameClick += OnNewGameClick;
+        _mainMenu.QuitClick += OnQuitClick;
+    }
+
+    private void OnCountinueClick()
+    {
+        _gameSceneManager.LoadLastGameScene();
+    }
+
+    private void OnNewGameClick()
+    {
+        var playerDefaultData = new PlayerPersistentData();
+        _dataManager.SaveData(playerDefaultData);
+        _gameSceneManager.LoadNextScene();
+    }
+    private void OnQuitClick()
+    {
+        Application.Quit();
+    }
 }
---
+++ Assets/Scripts/GameSceneManager.cs
@@ -0,0 +1,29 @@
+﻿
+
+using UnityEngine.SceneManagement;
+
+public class GameSceneManager //shitty class need rework
+{        
+    private const int MAIN_MENU_SCENE_INDEX = 0;
+    
+    public void LoadNextScene()
+    {
+        var dataManager = new DataManager();
+        var playerData = dataManager.LoadData();
+        ++playerData.SceneIndex;       
+        dataManager.SaveData(playerData);
+        SceneManager.LoadScene(playerData.SceneIndex);
+    }
+
+    public void LoadLastGameScene()
+    {
+        var dataManager = new DataManager();
+        var playerData = dataManager.LoadData();
+        SceneManager.LoadScene(playerData.SceneIndex);
+    }
+
+    public void MainMenuScene()
+    {
+        SceneManager.LoadScene(MAIN_MENU_SCENE_INDEX);
+    }
+}
