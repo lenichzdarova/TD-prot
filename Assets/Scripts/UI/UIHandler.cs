d1d044cbe4dc34ed8d3fcc90f9f9bc5696cbdfd3
lenichzdarova <lenichzdarova@gmail.com>
2023-01-22 23:13:18 +0300

    IplayerReady + TutorialUI classes.

--- Assets/Scripts/UI/UIHandler.cs
+++ Assets/Scripts/UI/UIHandler.cs
@@ -1,28 +1,37 @@
 using UnityEngine;
 using System.Collections.Generic;
 using System;
+using UnityEngine.UI;
 
 public class UIHandler : MonoBehaviour, IBuildUIProvider
 {
     public event Action<int> BuildingIndexSelected;
     public event Action SellTower;
-    public event Action PlayerReady;
+    public event Action PlayerReadyToGamepLay;
 
     [SerializeField] private BuildUI _buildUI;
     [SerializeField] private GoldUI _goldUI;
     [SerializeField] private HealthUI _healthUI;
     [SerializeField] private IngameMenuUI _ingameMenuUI;
+    [SerializeField] private IPlayerReady _playerReady;
     private IPlayerGoldProvider _playerGoldProvider;
     private Health _playerHealth;
 
     public void Initialize(IPlayerGoldProvider iPlayerGoldProvider, Health playerHealth)
     {
+        _playerReady.Initialize();
+        _playerReady.ReadyToPlay += OnPlayerReady;
         _playerGoldProvider = iPlayerGoldProvider;
         _playerHealth = playerHealth;
         //ingameMenuUI.Show();
         //goldUI.Init(iPlayerGoldProvider);
         //healthUI.Init(iPlayerHealthProvider);
         //buildUI.Hide();        
+    }
+
+    private void OnPlayerReady()
+    {
+        PlayerReadyToGamepLay?.Invoke();
     }
 
     #region BuildUISection
